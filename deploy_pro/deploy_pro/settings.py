0211cb17b1db7f0853fe016062858264d7c53555
kondashreeya <shreeyakonda@gmail.com>
2026-04-08 17:33:48 +0530

    first commit

--- deploy_pro/deploy_pro/settings.py
+++ deploy_pro/deploy_pro/settings.py
@@ -76,7 +76,7 @@ if os.environ.get("RENDER"):
             "ENGINE":"django.db.backends.mysql",
             "NAME":os.environ.get("MYSQLDATABASE"),
             "USER":os.environ.get("MYSQLUSER"),
-            "PASSWORD":os.environ.get("mysqlpassword"),
+            "PASSWORD":os.environ.get("MYSQLPASSWORD"),
             "HOST":os.environ.get("MYSQLHOST"),
             "PORT":os.environ.get("MYSQLPORT","3306")
 
